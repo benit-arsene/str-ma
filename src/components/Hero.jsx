43b0d93b11c19ec68be3f021b7ebc6847886bf13
LODD YRB <takeoffbenit123@gmail.com>
2026-09-03 14:16:28 +0200

    hgvk

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,15 +17,15 @@ function Hero() {
     {
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHNgxHPDleuzOt6LbOYkeBdBcWP-ltwlrbMPSij_NePSA9Ie4Whp6ow-g&s=10",
-      title: "Life is Short and",
-      highlight: "The World is Wide.",
+      title: "Kigali, Rwanda - You Will Visit Us Once",
+      highlight: "Come and See.",
       description:
         "Explore vibrant cities, breathtaking landscapes, and carefully planned adventures across China.",
     },
     {
       image: "https://locatify.com/wp-content/uploads/2023/11/1.jpg",
-      title: "Life is Short and",
-      highlight: "The World is Wide.",
+      title: "In this World",
+      highlight: "Good things are given .",
       description:
         "Discover London through iconic streets, rich history, and unforgettable cultural moments.",
     },
